bc5423f74a2d7e880ba5446a19664ea416ce7131
cpmorav <christian.morav@gmail.com>
2022-08-29 23:54:06 -0400

    agregando cambios y archivos de vistas

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>inicio clinica veterinaria</title>
+</head>
+<body>
+clinica veterinaria
+"pronto por aqui todo el sistema de administracion de clinica veterinaria"
+<ul>
+	<li>nuevo paciente(mascota)</li>
+	<li>agenda paciente</li>
+	<li>ver pacientes</li>
+	<li>administracion</li>
+</ul>
+</body>
+</html>
